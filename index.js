d8b4070bacea7dd5e0555dda1c12fd4d9786697a
Maxime Defrances <maxime.defrances@hotmail.fr>
2020-06-02 22:26:18 +0200

    check connexion details

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ const twitter_lite = require('twitter-lite');
 const fs = require('fs');
 var twitterListen = require('./twitter/twitterListenStream');
 
-let account_details = JSON.parse(fs.readFileSync('twitter/params/accounts_details.json'));
+//let account_details = JSON.parse(fs.readFileSync('twitter/params/accounts_details.json'));
 let subscribers = JSON.parse(fs.readFileSync('twitter/params/subscribers.json'));
 let content_providers = JSON.parse(fs.readFileSync('twitter/params/content_providers.json'));
 
-const client = new twitter_lite({
-    consumer_key: process.env.consumer_key || account_details.consumer_key,
-    consumer_secret: process.env.consumer_secret || account_details.consumer_secret,
-    access_token_key: process.env.access_token_key || account_details.access_token_key,
-    access_token_secret: process.env.access_token_secret || account_details.access_token_secret
-});
+// const client = new twitter_lite({
+//     consumer_key: process.env.consumer_key || account_details.consumer_key,
+//     consumer_secret: process.env.consumer_secret || account_details.consumer_secret,
+//     access_token_key: process.env.access_token_key || account_details.access_token_key,
+//     access_token_secret: process.env.access_token_secret || account_details.access_token_secret
+// });
+console.log("process.env.consumer_key : "+process.env.consumer_key)
+console.log("process.env.consumer_secret : "+process.env.consumer_secret)
+console.log("process.env.access_token_key : "+process.env.access_token_key)
+console.log("process.env.access_token_secret : "+process.env.access_token_secret)
 
-twitterListen.startListenTwitter(client,content_providers,subscribers);
+//twitterListen.startListenTwitter(client,content_providers,subscribers);
