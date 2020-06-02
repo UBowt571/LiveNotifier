bcfbd666d31096975bed7e6d62b83f2f2311dc71
Maxime Defrances <maxime.defrances@hotmail.fr>
2020-06-02 22:27:55 +0200

    test

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ let content_providers = JSON.parse(fs.readFileSync('twitter/params/content_provi
 //     access_token_key: process.env.access_token_key || account_details.access_token_key,
 //     access_token_secret: process.env.access_token_secret || account_details.access_token_secret
 // });
+console.log("details : ");
 console.log("process.env.consumer_key : "+process.env.consumer_key)
 console.log("process.env.consumer_secret : "+process.env.consumer_secret)
 console.log("process.env.access_token_key : "+process.env.access_token_key)
